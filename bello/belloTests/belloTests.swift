18881365503a456c47c2fbcc55b4224473635205
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-06-20 10:42:21 +0200

    xcode creation

---
+++ bello/belloTests/belloTests.swift
@@ -0,0 +1,17 @@
+//
+//  belloTests.swift
+//  belloTests
+//
+//  Created by Codgo on 20/6/25.
+//
+
+import Testing
+@testable import bello
+
+struct belloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
